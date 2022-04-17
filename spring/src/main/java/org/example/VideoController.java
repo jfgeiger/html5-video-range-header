d6dce791dbec0f70fa6284a55f05cd6d81247f2b
Johannes Geiger <jgeiger@opus2.com>
2022-04-17 12:29:17 +0100

    Remove redundant custom resource implementation

--- spring/src/main/java/org/example/VideoController.java
+++ spring/src/main/java/org/example/VideoController.java
@@ -2,9 +2,9 @@ package org.example;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Objects;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,17 +20,12 @@ public class VideoController {
   @GetMapping(value = VIDEO_MP4, produces = MEDIA_TYPE_VIDEO_MP4)
   public ResponseEntity<Resource> get() throws IOException {
     final var byteArrayOutputStream = new ByteArrayOutputStream();
-    final var inputStream = this.getInputStream();
+    final var inputStream = Objects.requireNonNull(
+        VideoController.class.getResourceAsStream(VIDEO_MP4));
     IOUtils.copy(inputStream, byteArrayOutputStream);
+    final var byteArray = byteArrayOutputStream.toByteArray();
+    final var byteArrayResource = new ByteArrayResource(byteArray);
 
-    final var contentLength = byteArrayOutputStream.size();
-    final var inputStreamResource = new HttpRangeInputStreamResource(this.getInputStream(),
-        contentLength);
-
-    return ResponseEntity.ok(inputStreamResource);
-  }
-
-  private InputStream getInputStream() {
-    return Objects.requireNonNull(VideoController.class.getResourceAsStream(VIDEO_MP4));
+    return ResponseEntity.ok(byteArrayResource);
   }
 }
